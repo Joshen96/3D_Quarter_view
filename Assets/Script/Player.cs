0cccc9066724d338ad00f03e5df5243a55c4a334
jeonhyewon26 <asdzxc250@naver.com>
2023-02-21 18:00:36 +0900

    무기변경스왑
    아이템먹으면튀어오르기하는중

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public bool[] hasWeapons;
 
 
+
     public float jumppower = 0f;
 
     static int jumpcount = 2;
@@ -23,11 +24,17 @@ public class Player : MonoBehaviour
     bool dodgeDown = false;
     bool iDown = false;
 
+    bool sDown1;
+    bool sDown2;
+    bool sDown3;
 
 
-    //점프 제약조건
-    bool isJump = false;
-    bool isDodge = false;
+
+
+    //제약조건
+    bool isJump ;
+    bool isDodge ;
+    bool isSwap;
 
     Vector3 moveVec;
 
@@ -36,6 +43,8 @@ public class Player : MonoBehaviour
     Animator anim;
 
     GameObject nearObject;
+    GameObject equipWeapon;
+    int equalsWeaponIndex = -1;
     void Awake()
     {
         anim = GetComponentInChildren<Animator>(); //자식오브젝트에 있는 컴포넌트 가저오는것
@@ -52,8 +61,43 @@ public class Player : MonoBehaviour
         Dodge();
         StopToWall(); // 레이캐스트 사용하여 레이어마스크 Wall 감지
         Interation();
+        Swap();
+    }
+    void Swap() 
+    {
+        
+        ///무기를 습득해야만 들수있게 + 같은무기 누르면 스왑안하게
+        if (sDown1 && (!hasWeapons[0] || equalsWeaponIndex == 0))  //1번을 눌럿을때 망치 소유x 나 1번을 눌럿을때는 스왑x
+            return;
+        if (sDown2 && (!hasWeapons[1] || equalsWeaponIndex == 1))
+            return;
+        if (sDown3 && (!hasWeapons[2] || equalsWeaponIndex == 2))
+            return;
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+        if (sDown3) weaponIndex = 2;
+
+
+        if ((sDown1|| sDown2 || sDown3) && !isJump && !isDodge)
+        {
+            if (equipWeapon != null) { equipWeapon.SetActive(false); }
+                isSwap = true;
+                equalsWeaponIndex = weaponIndex;
+                equipWeapon = weapons[weaponIndex];
+                equipWeapon.SetActive(true);
+
+                anim.SetTrigger("doSwap");
+                
+            Invoke(nameof(SwapOut), 0.5f);
+        }
     }
 
+    void SwapOut()
+    {
+        isSwap = false;
+    }
     void Interation()
     { 
         if(iDown&& nearObject != null&&!isJump&&!isDodge)
@@ -63,11 +107,17 @@ public class Player : MonoBehaviour
                 Item item = nearObject.GetComponent<Item>();
                 int weaponIndex = item.value;
                 hasWeapons[weaponIndex] = true;
+                nearObject.GetComponent<Rigidbody>();
+                nearObject.transform.position = Vector3.up * 10;
 
-                Destroy(nearObject.gameObject);
+                Invoke(nameof(DestroyDelay),0.5f);
             }
         }
 
+    }
+    void DestroyDelay()
+    {
+        Destroy(nearObject.gameObject);
     }
     void GetInput()
     {
@@ -77,10 +127,16 @@ public class Player : MonoBehaviour
         jumpDown = Input.GetButtonDown("Jump");
         dodgeDown = Input.GetButtonDown("Fire1");
         iDown = Input.GetButtonDown("Interation");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+        sDown3 = Input.GetButtonDown("Swap3");
+
 
 
 
     }
+
+    
     void Move()
     {
         if (isTouchWall) // 벽탐지하면 멈춤
@@ -96,7 +152,10 @@ public class Player : MonoBehaviour
 
                 moveVec = new Vector3(hAxis, 0, vAxis).normalized;
             }
-
+            if (isSwap)
+            {
+                moveVec = Vector3.zero;
+            }
                 // if문 사용시
                 /*
 
@@ -130,7 +189,7 @@ public class Player : MonoBehaviour
         void Jump()
         {
 
-            if (jumpDown && !isJump && !isDodge &&jumpcount>0)
+            if (jumpDown && !isJump && !isDodge &&jumpcount>0 && !isSwap)
             {
                 jumpcount--;
                 //Debug.Log("점프");
@@ -144,7 +203,7 @@ public class Player : MonoBehaviour
         void Dodge()
         {
 
-            if (dodgeDown && !isDodge && moveVec != Vector3.zero)
+            if (dodgeDown && !isDodge && moveVec != Vector3.zero&&!isSwap)
             {
 
                 //Debug.Log("구르기");
