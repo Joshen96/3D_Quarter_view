140da253b96256e88309302e165e5bcad2243b68
jeonhyewon26 <asdzxc250@naver.com>
2023-02-21 14:45:15 +0900

    아이템해머 생성

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -9,23 +9,38 @@ public class Item : MonoBehaviour
     public int value;
     float count = 0f;
 
+    float updownT = 2;
+
+    
+
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * 100 * Time.deltaTime);
-        updown();
 
+        
+        updown();
+        Debug.Log(count);
     }
     void updown()
     {
+        count += Time.deltaTime;
+
         if (count < 1f)
         {
-            count += Time.deltaTime;
-            transform.position += Vector3.up * 1 * Time.deltaTime;
+           
+            transform.position += Vector3.up * 1.5f * Time.deltaTime;
             
         }
-       
-        
-
+        else if(count > 1f && count<2f){
+           
+            transform.position += Vector3.down * 1.5f * Time.deltaTime;
+            
+        }
+        else
+        {
+            count = 0;
+        }
     }
+    
 }
